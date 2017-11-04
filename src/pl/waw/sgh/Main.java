dfdc2ae5a72208dd27a6a3f8d99f30c9acab5e4e
ms73995 <szczepanskamagdalena6@gmail.com>
2017-11-04 13:18:21 +0100

    1

--- src/pl/waw/sgh/Main.java
+++ src/pl/waw/sgh/Main.java
@@ -3,7 +3,53 @@ package pl.waw.sgh;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Witam!!!");
+
+        int a = 5;
+        Integer aa = 10;
+
+        Double d;
+        d = 5d;
+
+        d = d + a + aa;
+        System.out.println("d=" + d);
+
+        String s1 = "abc";
+        String s2 = "def";
+        String s3 = sl.concat(s2);
+
+        System.out.println(s3);
+
+        boolean b = false;
+        boolean c = true;
+
+        //AND &&
+        //OR ||
+        //XOR ^
+
+        boolean r = b ^ c;
+
+        int j = 8;
     }
+
+    int 1=6;
+    System.out.println(i);
+    System.out.println(j);
+
+
+    //System.out.println (i);
+    System.out.println(j);
+
+    if(r)
+
+    {
+        System.out.println("r jest prawda");
+    } else
+            System.out.println("r jest fałszem");
+
+    String s = "AB";
+
+
 }
+
